d50f141bfb8b6b8f759db66caedce4ca227babbf
seazas222 <seazas222@gmail.com>
2026-07-13 17:24:08 +0700

    AA

--- my_app/lib/widgits/activity_list_widget.dart
+++ my_app/lib/widgits/activity_list_widget.dart
@@ -3,4 +3,21 @@ import 'package:my_app?models/activity_model.dart';
 
 class ActivityListWidget extends StatelessWidget {
     const ActivityListWidget({super.key, required this.activityModeStore});
+    final List<ActivityModel> activityModeStore;
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            padding: EdgeInsets.all(8),
+            height: 400,
+            child: ListView.builder(
+                itemCount: activityModeStore.length,
+                itemBuilder: (context, index) {
+                    ActivityModel item = activityModeStore[index];
+
+                    returnn ListTile(title: Text(item.activityDate));
+                },
+            ),
+        );
+    }
 }
